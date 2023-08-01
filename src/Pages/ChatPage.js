151f6645f2dba34de2d8671aa9c1f22fb0cda016
NatashaSharif <natashasharif7272@gmail.com>
2023-08-01 19:15:15 +0500

    Initial code

---
+++ src/Pages/ChatPage.js
@@ -0,0 +1,17 @@
+import React, { useEffect, useState } from "react";
+import { ChatState } from "../Context/ChatProvider";
+
+
+const ChatPage = () => {
+
+  const {user} = ChatState();
+ 
+  return (
+    <div style={{width:"100%"}}>
+
+      
+    </div>
+  );
+};
+
+export default ChatPage;
---
+++ src/Pages/HomePage.js
@@ -0,0 +1,65 @@
+import {
+  Box,
+  Container,
+  Text,
+  Tab,
+  TabList,
+  TabPanel,
+  TabPanels,
+  Tabs,
+} from "@chakra-ui/react";
+import React from "react";
+import Login from "../components/Authentication/Login";
+import Signup from "../components/Authentication/Signup";
+import { useHistory } from "react-router-dom";
+import { useEffect, useState } from "react";
+
+const HomePage = () => {
+  const history = useHistory();
+  const [user, setUser] = useState();
+
+
+  useEffect(() => {
+    const userInfo = JSON.parse(localStorage.getItem("userInfo"));
+    setUser(userInfo);
+
+    if (!userInfo) history.push("/chats");
+  }, [history]);
+
+  return (
+    <Container maxW="xl" centerContent>
+      <Box
+        d="flex"
+        justifyContent="center"
+        p={3}
+        bg="transparent"
+        w="100%"
+        m="40px 0 15px 0"
+        borderRadius="lg"
+        borderWidth="1px"
+      >
+        <Text fontSize="4xl" fontFamily="Work sans">
+          Talk-A-Tive
+        </Text>
+      </Box>
+      <Box bg="transparent" w="100%" p={4} borderRadius="lg" borderWidth="1px">
+        <Tabs isFitted variant="soft-rounded">
+          <TabList mb="1em">
+            <Tab width="50%">Login</Tab>
+            <Tab width="50%">Sign Up</Tab>
+          </TabList>
+          <TabPanels>
+            <TabPanel>
+              <Login />
+            </TabPanel>
+            <TabPanel>
+              <Signup />
+            </TabPanel>
+          </TabPanels>
+        </Tabs>
+      </Box>
+    </Container>
+  );
+};
+
+export default HomePage;
